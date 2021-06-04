d07c4be01bb7afc09b7e58023a616a3ad897d132
KhlebAnton <toni.farsh@icloud.com>
2021-06-04 13:17:52 +0500

    add filea

---
+++ src/main/java/Main.java
@@ -0,0 +1,56 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json);
+
+
+    }
+
+    private static void writeString(String json) {
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static <T> String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<T>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            csv.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
